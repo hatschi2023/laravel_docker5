ef6268dd601df4b776b41e09a55a559f01c2e161
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-09-08 17:29:58 +0900

    auth_login

--- src/Reserve/resources/views/auth/login.blade.php
+++ src/Reserve/resources/views/auth/login.blade.php
@@ -1,13 +1,17 @@
 <x-guest-layout>
     <x-jet-authentication-card>
+
+        <!-- Logo -->
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <div class="w-40">
+                <a href="/">
+                    <x-jet-authentication-card-logo />
+                </a>
+            </div>
         </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
-
         @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
+            <div class="px-4 py-4 text-sm font-medium text-green-600 bg-green-50">
                 {{ session('status') }}
             </div>
         @endif
@@ -16,31 +20,31 @@
             @csrf
 
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <x-jet-label for="email" value="{{ __('メールアドレス') }}" />
+                <x-jet-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-label for="password" value="{{ __('パスワード') }}" />
+                <x-jet-input id="password" class="block w-full mt-1" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('パスワードを保存する') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                <a class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('パスワードを忘れたとき') }}
+                </a>
                 @endif
 
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
+                <x-jet-button class="ml-4 tx-s">
+                    {{ __('ログイン') }}
                 </x-jet-button>
             </div>
         </form>
